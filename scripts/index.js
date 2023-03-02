e0e094dbf05ccbb24f4a0a747ddf0654a21874b8
Anna Bazhenova <shchapinaa@gmail.com>
2023-03-02 10:55:59 +0100

    add profile form edit functionality

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,21 @@ const closeButton = document.querySelector('.form__close-button');
 editButton.addEventListener('click', () => popup.classList.add('popup_opened'));
 closeButton.addEventListener('click', () => popup.classList.remove('popup_opened'));
 
+const formElement = document.querySelector('.form');
+const nameInput = document.querySelector('.form__input_type_name');
+const jobInput = document.querySelector('.form__input_type_text');
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    let nameValue = nameInput.value;
+    let jobValue = jobInput.value;
+
+    let profileName = document.querySelector('.profile__name');
+    let profileJob = document.querySelector('.profile__profession');
+
+    profileName.textContent = nameValue;
+    profileJob.textContent = jobValue;
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
