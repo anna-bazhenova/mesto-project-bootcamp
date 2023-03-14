8120d974732b90c9dc789488844968545aef6d26
Anna Bazhenova <shchapinaa@gmail.com>
2023-03-14 20:35:04 +0100

    configure webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './pages/index.css';
 const editButton = document.querySelector('.profile__edit-button');
 const popupEditForm = document.querySelector('.popup_edit-form');
 const profileName = document.querySelector('.profile__name');
@@ -217,64 +218,3 @@ const toggleSubmitButtonState = (form, config) => {
 };
 
 enableValidation(validationConfig);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const isValid = (formElement, inputElement) => {
-//   if (!inputElement.validity.valid) {
-//     // showInputError теперь получает параметром форму, в которой
-//     // находится проверяемое поле, и само это поле
-//     showInputError(formElement, inputElement, inputElement.validationMessage);
-//   } else {
-//     // hideInputError теперь получает параметром форму, в которой
-//     // находится проверяемое поле, и само это поле
-//     hideInputError(formElement, inputElement);
-//   }
-// };
-
-// const showInputError = (formElement, inputElement, errorMessage) => {
-//   // Находим элемент ошибки внутри самой функции
-//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//   // Остальной код такой же
-//   inputElement.classList.add('form__input_type_error');
-//   errorElement.textContent = errorMessage;
-//   errorElement.classList.add('form__input-error_active');
-// };
-
-// const hideInputError = (formElement, inputElement) => {
-//   // Находим элемент ошибки
-//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//   // Остальной код такой же
-//   inputElement.classList.remove('form__input_type_error');
-//   errorElement.classList.remove('form__input-error_active');
-//   errorElement.textContent = '';
-// };
-
-// const setEventListeners = (formElement) => {
-//   // Находим все поля внутри формы,
-//   // сделаем из них массив методом Array.from
-//   const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-
-//   // Обойдём все элементы полученной коллекции
-//   inputList.forEach((inputElement) => {
-//     // каждому полю добавим обработчик события input
-//     inputElement.addEventListener('input', () => {
-//       // Внутри колбэка вызовем isValid,
-//       // передав ей форму и проверяемый элемент
-//       isValid(formElement, inputElement)
-//     });
-//   });
-// };
